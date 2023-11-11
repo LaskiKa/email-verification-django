d8281f34a5183370d907b7f4d8a1f8e08feffa52
Kamil Łaski <kamil.laski0@gmail.com>
2023-11-11 21:02:25 +0100

    JS Form validation 3

--- email_verification_project/email_verification_app/static/form_validation.js
+++ email_verification_project/email_verification_app/static/form_validation.js
@@ -3,11 +3,13 @@ const formValidation = () => {
   const nameInput = document.querySelector("#user_name");
   const surnamenameInput = document.querySelector("#user_surname");
   const emailInput = document.querySelector("#typeEmailX");
+  const passwordInput = document.querySelector("#typePasswordX");
   const nameInputStatus = nameInput.classList.contains("bg-success");
-  const surnamenameInputStatus = surnamenameInput.classList.contains("bg-success")
-  const emailInputStatus = emailInput.classList.contains("bg-success")
+  const surnamenameInputStatus = surnamenameInput.classList.contains("bg-success");
+  const emailInputStatus = emailInput.classList.contains("bg-success");
+  const passwordInputStatus = passwordInput.classList.contains("bg-success");
 
-  if (nameInputStatus && surnamenameInputStatus && emailInputStatus) {
+  if (nameInputStatus && surnamenameInputStatus && emailInputStatus && passwordInputStatus) {
     registerButton.disabled = false;
   } else {
     registerButton.disabled = true;
@@ -44,8 +46,6 @@ const surnameValidation = () => {
   const surnamenameInput = document.querySelector("#user_surname");
   const isMin = surnamenameInput.value.length > 1;
   const statusSuccess = surnamenameInput.classList.contains("bg-success");
-
-
 
   if(!isMin) {
     if (statusSuccess) {
@@ -90,4 +90,82 @@ const emailValidation = () => {
     emailInput.classList.replace("bg-danger", "bg-success");
   }
   formValidation()
+}
+
+const passwordValidation = () => {
+  const passwordInput = document.querySelector("#typePasswordX");
+  const lowerCaseDiv = document.querySelector("#lowercase");
+  const upperCaseDiv = document.querySelector("#uppercase");
+  const numberDiv = document.querySelector("#number");
+  const lengthDiv = document.querySelector("#length")
+
+  // lowercase
+  const lowerCaseReg = /[a-z]/g;
+  if (passwordInput.value.match(lowerCaseReg)) {
+    lowerCaseDiv.classList.remove("bg-danger");
+    lowerCaseDiv.classList.add("bg-success")
+  } else {
+    lowerCaseDiv.classList.remove("bg-success");
+    lowerCaseDiv.classList.add("bg-danger")
+  }
+
+  // uppercase
+  const upperCase = /[A-Z]/g;
+  if (passwordInput.value.match(upperCase)) {
+    upperCaseDiv.classList.remove("bg-danger");
+    upperCaseDiv.classList.add("bg-success")
+  } else {
+    upperCaseDiv.classList.remove("bg-success");
+    upperCaseDiv.classList.add("bg-danger")
+  }
+  // number
+  const numbers = /[0-9]/g;
+  if (passwordInput.value.match(numbers)) {
+    numberDiv.classList.remove("bg-danger");
+    numberDiv.classList.add("bg-success")
+  } else {
+    numberDiv.classList.remove("bg-success");
+    numberDiv.classList.add("bg-danger")
+  }  
+  
+  // minimum 8 characters
+  if (passwordInput.value.length >= 8) {
+    lengthDiv.classList.remove("bg-danger");
+    lengthDiv.classList.add("bg-success");
+  } else {
+    lengthDiv.classList.remove("bg-success");
+    lengthDiv.classList.add("bg-danger");
+  }
+
+  if (lowerCaseDiv.classList.contains("bg-success") &&
+      upperCaseDiv.classList.contains("bg-success") &&
+      numberDiv.classList.contains("bg-success") &&
+      lengthDiv.classList.contains("bg-success")) {
+
+        passwordInput.classList.add("bg-success");
+        passwordInput.classList.remove("bg-danger");
+  
+      } else {
+        passwordInput.classList.remove("bg-success");
+        passwordInput.classList.add("bg-danger")
+      }
+
+  formValidation()
+}
+
+const showRequirements = () => {
+  const passwordRequrements = document.querySelector("#pwd-requirements");
+  passwordRequrements.hidden = false;
+
+}
+
+const hideRequirementsifEmpty = () => {
+  const passwordInput = document.querySelector("#typePasswordX");
+  const passwordRequrements = document.querySelector("#pwd-requirements");
+
+  if ( passwordInput.value == "") {
+    passwordRequrements.hidden = true;  
+
+  }
+
 }
